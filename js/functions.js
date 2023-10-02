f4cdca24b32d619f653ff97174a7902237b3f0c5
Elizaveta <bonjuk.liza@yandex.ru>
2023-10-02 20:32:30 +0500

    Написаны функции

--- js/functions.js
+++ js/functions.js
@@ -2,9 +2,9 @@ const checkLength = (line, maxLenght) => line.length <= maxLenght;
 
 const isPalindrome = (line) => {
   const normalizedLine = line.replaceAll(' ', '').toLowerCase();
-  const reversedLine = normalizedLine.split('').reverse().join('');;
+  const reversedLine = normalizedLine.split('').reverse().join('');
   return reversedLine === normalizedLine;
-}
+};
 
 checkLength('проверяемая строка', 20);
 checkLength('проверяемая строка', 18);
